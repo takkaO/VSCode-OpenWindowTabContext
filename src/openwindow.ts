03dbc95c9f146dd187d65417175c12785c8b02b9
VirtualHomePC <None>
2021-09-27 21:49:51 +0900

    [update] implement AutoClose tab (only active column)
    - implement AutoClose tab (only active column)
    - fix setting parameter scope
    - update some packages

--- src/openwindow.ts
+++ src/openwindow.ts
@@ -6,9 +6,34 @@ import cp = require('child_process');
 
 export const OpenFileInNewWindowCommandId = 'open-window-tab-context.openFileInNewWindow';
 
-export function openFileInNewWindow(context?: any) {
+export async function openFileInNewWindow(context?: any) {
 	if (context instanceof vscode.Uri) {
 		launchNewInstance(context.fsPath);
+
+		if (vscode.workspace.getConfiguration().get("owtc.autoClose") === "Disable") {
+			// If disable auto close mode, finish all.
+			return;
+		}
+
+		let activeEditor = vscode.window.activeTextEditor;
+		let activeFilePath = activeEditor?.document.uri.fsPath;
+		if (activeFilePath === undefined) {
+			// Probably the process here will not be executed.
+			return;
+		}
+
+		if (activeFilePath === context.fsPath) {
+			// When the active tab and the tab opened in a new window are the same
+			vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+		}
+		else {
+			// When the active tab and the tab opened in a new window are different
+			let tmp = vscode.Uri.file(activeFilePath);                                                     // Backup the current active editor
+			await vscode.commands.executeCommand<vscode.TextDocumentShowOptions>("vscode.open", context);  // Activate the Open in New Window editor
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');                    // close active editor
+			vscode.commands.executeCommand<vscode.TextDocumentShowOptions>("vscode.open", tmp);            // Restore the active editor from a backup
+		}
+
 	} else {
 		vscode.window.showWarningMessage('Command failed. There is not a file.');
 	}
@@ -21,7 +46,7 @@ function launchNewInstance(filepath: string) {
 	if (codeCommand) {
 		filepath = "\"" + filepath + "\"";
 		codeCommand = "\"" + codeCommand + "\"";
-		cp.execFile(codeCommand, ['-n', filepath], {shell: true});
+		cp.execFile(codeCommand, ['-n', filepath], { shell: true });
 	} else {
 		vscode.window.showErrorMessage('Failed to fetch VScode execute path.');
 	}
