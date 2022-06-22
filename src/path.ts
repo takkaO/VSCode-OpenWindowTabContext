9e4c43730b34de519a607932617e4c0f22c15af8
work-desktop <work@desktop>
2022-06-22 17:27:30 +0900

    [update] fix failed to get command path in Mac (#16)

--- src/path.ts
+++ src/path.ts
@@ -26,11 +26,16 @@ export function getCodeCommandPath() {
 		candidate_paths.push(code_path);
 	}
 	// candidate 3
-	code_path = path.join(process.cwd(), command);
+	code_path = path.join(path.dirname(process.execPath), "../../../../Resources/app/bin", command);
 	if (fs.existsSync(code_path)) {
 		candidate_paths.push(code_path);
 	}
 	// candidate 4
+	code_path = path.join(process.cwd(), command);
+	if (fs.existsSync(code_path)) {
+		candidate_paths.push(code_path);
+	}
+	// candidate 5
 	code_path = path.join(process.cwd(), "bin", command);
 	if (fs.existsSync(code_path)) {
 		candidate_paths.push(code_path);
